80a7c7e2b462ef42b36fd44e6dfec62e889dab55
PutraNasri <nasrisyawaldi@gmail.com>
2017-01-24 09:27:03 +0700

    24117

---
+++ app/src/main/java/com/example/byonge/pdamtirtaaneuklaotkotasabang/jadwal.java
@@ -0,0 +1,14 @@
+package com.example.byonge.pdamtirtaaneuklaotkotasabang;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class jadwal extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jadwal);
+    }
+}
--- app/src/main/java/com/example/byonge/pdamtirtaaneuklaotkotasabang/pasang_baru.java
+++ app/src/main/java/com/example/byonge/pdamtirtaaneuklaotkotasabang/pasang_baru.java
@@ -1,8 +1,10 @@
 package com.example.byonge.pdamtirtaaneuklaotkotasabang;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class pasang_baru extends Activity {
 
@@ -10,6 +12,10 @@ public class pasang_baru extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pasang_baru);
+    }
+    public void back(View v){
+        Intent hasilIntent = new Intent(pasang_baru.this, pelanggan.class);
+        startActivity(hasilIntent);
     }
 }
 //assasa
