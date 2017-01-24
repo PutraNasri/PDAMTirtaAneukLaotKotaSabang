c58800c3f2eee1dae362ddde06661ccf5030508f
PutraNasri <nasrisyawaldi@gmail.com>
2017-01-24 09:13:16 +0700

    24117

---
+++ app/src/main/java/com/example/byonge/pdamtirtaaneuklaotkotasabang/pasang_baru.java
@@ -0,0 +1,14 @@
+package com.example.byonge.pdamtirtaaneuklaotkotasabang;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pasang_baru extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pasang_baru);
+    }
+}
