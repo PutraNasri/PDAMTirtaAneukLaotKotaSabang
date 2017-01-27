0bd2a099c48feab5b9c967181f7684d3c324bc8b
PutraNasri <nasrisyawaldi@gmail.com>
2017-01-27 11:15:16 +0700

    27117

--- app/src/main/java/com/example/byonge/pdamtirtaaneuklaotkotasabang/config.java
+++ app/src/main/java/com/example/byonge/pdamtirtaaneuklaotkotasabang/config.java
@@ -6,40 +6,16 @@ package com.example.byonge.pdamtirtaaneuklaotkotasabang;
 public class config {
     //String Alamat untuk CRUD
     public static final String URL_ADD_KELUHAN="http://cobabflf.esy.es/addkeluhan.php";
+    public static final String URL_ADD_PASANG_BARU="http://cobabflf.esy.es/pasang_baru.php";
     public static final String URL_GET_BERITA="http://cobabflf.esy.es/getberita.php";
     public static final String URL_GET_DETAIL_BERITA = "http://cobabflf.esy.es/get_detail_berita.php?id=";
     public static final String URL_GET_ALL_LOCATION="http://kinketkuena.esy.es/getAlllocation.php";
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //syahkuala
-    public static final String URL_GET_ALL = "http://kinketkuena.esy.es/getAllEmp.php";
-    public static final String URL_GET_EMP = "http://kinketkuena.esy.es/getEmp.php?id=";
-    //kutaraja
-    public static final String URL_GET_ALL_KUTARAJA="http://kinketkuena.esy.es/getAllKutaraja.php";
-    public static final String URL_GET_KUTARAJA="http://kinketkuena.esy.es/getKutaraja.php?id=";
-    //ulekareng
-    public static final String URL_GET_ALL_ULEKARENG="http://kinketkuena.esy.es/getAllUlekareng.php";
-    public static final String URL_GET_ULEKARENG="http://kinketkuena.esy.es/getUlekareng.php?id=";
-    //bandaraya
-    public static final String URL_GET_ALL_BANDARAYA="http://kinketkuena.esy.es/getAllBandaraya.php";
-    public static final String URL_GET_BANDARAYA="http://kinketkuena.esy.es/getBandaraya.php?id=";
-    //jayabaru
-    public static final String URL_GET_ALL_JAYABARU="http://kinketkuena.esy.es/getAllJayabaru.php";
-    public static final String URL_GET_JAYABARU="http://kinketkuena.esy.es/getJayabaru.php?id=";
-    //kutaalam
-    public static final String URL_GET_ALL_KUTAALAM="http://kinketkuena.esy.es/getAllKutaalam.php";
-    public static final String URL_GET_KUTAALAM="http://kinketkuena.esy.es/getKutaalam.php?id=";
-    //luengbata
-    public static final String URL_GET_ALL_LUENGBATA="http://kinketkuena.esy.es/getAllLuengbata.php";
-    public static final String URL_GET_LUENGBATA="http://kinketkuena.esy.es/getLuengbata.php?id=";
-    //meuraxa
-    public static final String URL_GET_ALL_MEURAXA="http://kinketkuena.esy.es/getAllMeuraxa.php";
-    public static final String URL_GET_MEURAXA="http://kinketkuena.esy.es/getMeuraxa.php?id=";
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-   // public static final String URL_UPDATE_EMP = "http://192.168.94.1/Android/CRUD/updateEmp.php";
-    //public static final String URL_DELETE_EMP = "http://192.168.94.1/Android/CRUD/deleteEmp.php?id=";
 
     //Kunci untuk melakukan pemanggilan data dari scrip
     public static final String KEY_EMP_ID = "id";
@@ -50,11 +26,13 @@ public class config {
     public static final String KEY_EMP_ISI = "isi";
 
 
-
-    public static final String KEY_EMP_NAME = "namakost";
-    public static final String KEY_EMP_DESG = "namapemilik";
-    public static final String KEY_EMP_SAL = "nohp";
-    public static final String KEY_EMP_NIK = "nik";
+    public static final String KEY_EMP_NAMA_BARU = "nama";
+    public static final String KEY_EMP_IDENTITAS_BARU = "identitas";
+    public static final String KEY_EMP_PEKERJAAN_BARU = "pekerjaan";
+    public static final String KEY_EMP_BANGUNAN_BARU = "bangunan";
+    public static final String KEY_EMP_ALAMAT_BARU = "alamat";
+    public static final String KEY_EMP_KONTAK_BARU = "kontak";
+    public static final String KEY_EMP_ALAMAT_PASANG_BARU = "alamat_pasang";
     //JSON Tags
 
 
--- app/src/main/java/com/example/byonge/pdamtirtaaneuklaotkotasabang/pasang_baru.java
+++ app/src/main/java/com/example/byonge/pdamtirtaaneuklaotkotasabang/pasang_baru.java
@@ -1,29 +1,106 @@
 package com.example.byonge.pdamtirtaaneuklaotkotasabang;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.NetworkOnMainThreadException;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.HashMap;
 
 public class pasang_baru extends Activity {
-
+    private EditText editTextnama;
+    private EditText editTextidentitas;
+    private EditText editTextpekerjaan;
+    private EditText editTextbangunan;
+    private EditText editTextalamat;
+    private EditText editTextkontak;
+    private EditText editTextalamatpasang;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pasang_baru);
+        editTextnama = (EditText) findViewById(R.id.editnama);
+        editTextidentitas = (EditText) findViewById(R.id.editidentitas);
+        editTextpekerjaan = (EditText) findViewById(R.id.editpekerjaan);
+        editTextbangunan = (EditText) findViewById(R.id.editbangunan);
+        editTextalamat = (EditText) findViewById(R.id.editalamat);
+        editTextkontak = (EditText) findViewById(R.id.editkontak);
+        editTextalamatpasang = (EditText) findViewById(R.id.editalamatpasang);
     }
     public void back(View v){
         Intent hasilIntent = new Intent(pasang_baru.this, pelanggan.class);
         startActivity(hasilIntent);
         finish();
     }
-
+    public void send(View v){
+        addEmployee();
+    }
+    private void addEmployee() {
+        //proses pengambilan string dari variabel
+        final String nama = editTextnama.getText().toString().trim();
+        final String identitas = editTextidentitas.getText().toString().trim();
+        final String pekerjaan = editTextpekerjaan.getText().toString().trim();
+        final String bangunan = editTextbangunan.getText().toString().trim();
+        final String alamat = editTextalamat.getText().toString().trim();
+        final String kontak = editTextkontak.getText().toString().trim();
+        final String alamat_pasang = editTextalamatpasang.getText().toString().trim();
+        class AddEmployee extends AsyncTask<Void, Void, String> {
+            ProgressDialog loading;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(pasang_baru.this, "Mengirim Data ...", "HARAP TUNGGU...", false, false);
+            }
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(pasang_baru.this, s, Toast.LENGTH_LONG).show();
+                AlertDialog.Builder a_builder = new AlertDialog.Builder(pasang_baru.this);
+                a_builder.setMessage("Terima Kasih Telah Menggunakan Layanan Kami,Pengajuan Pasang Baru anda segera di proses")
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                                Intent hasilIntent = new Intent(pasang_baru.this, MainActivity.class);
+                                startActivity(hasilIntent);
+                                finish();
+                            }
+                        });
+                AlertDialog alert = a_builder.create();
+                alert.setTitle("Info");
+                alert.show();
+            }
+            @Override
+            protected String doInBackground(Void... v) {
+                HashMap<String, String> params = new HashMap<>();
+                params.put(config.KEY_EMP_NAMA_BARU, nama);
+                params.put(config.KEY_EMP_IDENTITAS_BARU, identitas);
+                params.put(config.KEY_EMP_PEKERJAAN_BARU, pekerjaan);
+                params.put(config.KEY_EMP_BANGUNAN_BARU, bangunan);
+                params.put(config.KEY_EMP_ALAMAT_BARU, alamat);
+                params.put(config.KEY_EMP_KONTAK_BARU, kontak);
+                params.put(config.KEY_EMP_ALAMAT_PASANG_BARU, alamat_pasang);
+                RequestHandler rh = new RequestHandler();
+                String res = rh.sendPostRequest(config.URL_ADD_PASANG_BARU, params);
+                return res;
+            }
+        }
+        AddEmployee ae = new AddEmployee();
+        ae.execute();
+    }
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if ((keyCode == KeyEvent.KEYCODE_BACK)) {
-
             return false;
         }
         return super.onKeyDown(keyCode, event);
